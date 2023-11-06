252ba4f7d2e2c1807e0164e70ad78730e45bd8cb
Heloez <escolaanaehelo@gmail.com>
2023-11-06 09:40:34 -0300

    06/11

--- MtecDevs/Controllers/AccountController.cs
+++ MtecDevs/Controllers/AccountController.cs
@@ -1,26 +1,61 @@
+using System.Net.Mail;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
+using MtecDevs.ViewModels;
 
 namespace MtecDevs.Controllers;
 
     public class AccountController : Controller
     {
         private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<IdentityUser> _sigInManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
+        public AccountController(
+            ILogger<AccountController> logger,
+            SignInManager<IdentityUser> sigInManager,
+            UserManager<IdentityUser> userManager)
+            {
+                _logger = logger;
+                _sigInManager = sigInManager;
+                _userManager = userManager;
+            }
+        
         public AccountController(ILogger<AccountController> logger)
         {
             _logger = logger;
         }
        [HttpGet]
-        public IActionResult Login()
+        public IActionResult Login(string returnUrl)
         {
-            return View();
+           LoginVM loginVM = new(){
+            UrlRetorno = returnUrl ?? Url.Content("~/")
+           };
+            return View(loginVM);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+       [HttpPost]
+       [ValidateAntiForgeryToken]
+
+       public async Task<IActionResult> login(LoginVM login)
+       {
+        if (ModelState.IsValid)
         {
-            return View("Error!");
+
         }
-    
+        return View(login);
+       }  
+
+       private  static bool IsValidEmail(string email)
+       {
+        try
+        {
+            MailAddress m = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+       }
 }
